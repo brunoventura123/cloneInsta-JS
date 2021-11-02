871dce3db92492e92cad4fcbb814d627aac94838
Bruno Leonardo Sancho Ventura <86660522+brunoventura123@users.noreply.github.com>
2021-11-02 13:22:43 -0300

    first commit

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var openMenu = function () { return toggleMenu(document.querySelector('.up-box-1')); };
-var follow = function () { return toggleMenu(document.querySelector('.up-box')); };
-var slideMove = document.querySelector('.slider-move');
-var modal = document.querySelector('.modal');
-var modalp = document.querySelector('.modalp');
+let openMenu = function () { return toggleMenu(document.querySelector('.up-box-1')); };
+let follow = function () { return toggleMenu(document.querySelector('.up-box')); };
+let slideMove = document.querySelector('.slider-move');
+let modal = document.querySelector('.modal');
+let modalp = document.querySelector('.modalp');
 function toggleMenu(e) {
     e.classList.toggle('open');
 }
 function friends() {
-    var menu = document.querySelector('.friends');
+    let menu = document.querySelector('.friends');
     menu.classList.toggle('openFriends');
 }
 function move(e) {
     slideMove.style.marginLeft = e;
 }
 function showVideo(e) {
-    var video = document.querySelector('.modalp video');
+    let video = document.querySelector('.modalp video');
     modalp.style.opacity = '0';
     modalp.style.display = 'flex';
     video.setAttribute('src', 'images/videos/' + e + '.mp4');
@@ -23,7 +23,7 @@ function showVideo(e) {
     }, 200);
 }
 function showPhoto(e) {
-    var photo = document.querySelector('.modal img');
+    let photo = document.querySelector('.modal img');
     modal.style.opacity = '0';
     modal.style.display = 'flex';
     photo.setAttribute('src', 'images/photos/' + e + '.png');
@@ -31,15 +31,15 @@ function showPhoto(e) {
         modal.style.opacity = '1';
     }, 200);
 }
-var qt = 17; // Limite a mostrar
-var reels = document.querySelector('.rells');
-var num = 27; // Limite a mostrar
-var area = document.querySelector('.posts');
+let qt = 17; // Limite a mostrar
+let reels = document.querySelector('.rells');
+let num = 27; // Limite a mostrar
+let area = document.querySelector('.posts');
 function loading() {
-    for (var i = 1; i <= num; i++) {
+    for (let i = 1; i <= num; i++) {
         area.innerHTML += "<img onClick=\"showPhoto(" + i + ")\" src='images/photos/" + i + ".png'>";
     }
-    for (var i = 1; i <= qt; i++) {
+    for (let i = 1; i <= qt; i++) {
         reels.innerHTML += "<video onClick=\"showVideo(" + i + ")\" src='images/videos/" + i + ".mp4'>";
     }
 }
